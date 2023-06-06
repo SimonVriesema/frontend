39c3701bdc005dc37c7494130dac48a8e1fb9597
Simon Vriesema <simon.vriesema@icloud.com>
2023-06-06 23:26:15 +0200

    💡 [Feat]: Setup the frontend environment
    📝 Frontend environment so I can start working
    
    🚀 Motivatie:
    Zodat ik aan de slag kan

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+    <h1 class="text-red-500">Fick dich, du bist eine schweinhund</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      // Component data goes here
+    };
+  },
+  props: {
+    // Component props go here
+  },
+  computed: {
+    // Computed properties go here
+  },
+  methods: {
+    // Component methods go here
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
